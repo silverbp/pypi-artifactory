b2cd0c122188d5635db0a520edf7743b000eb574
Casey Entzi <casey.entzi@icloud.com>
2017-03-03 16:52:46 -0700

    updating

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ Setup.py
 
 from setuptools import setup
 
-setup(name='silverbp_artifactory',
+setup(name='silverbp-artifactory',
       version='0.1',
       description='Python API for interacting with Artifactory',
       url='https://github.com/silverbp/pypi-artifactory',
